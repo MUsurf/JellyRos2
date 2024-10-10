47d25af8ae4807fe6ac80d959df6be452c230032
Max Welge <max@mwc-053252.dhcp.missouri.edu>
2024-10-10 18:19:26 -0500

    10/10
    my first commit

--- catkin_ws/src/motor_control/src/motor_driver.py
+++ catkin_ws/src/motor_control/src/motor_driver.py
@@ -14,7 +14,6 @@ Publishes:
 
 Subscribes:
 ----------
-
 '''
 
 
@@ -53,9 +52,9 @@ def motor_driver():
 
     index = 0
 
-    # pub = rospy.Publisher('motor_command', Int32MultiArray, queue_size=10)
-    # rospy.init_node('motor_driver', anonymous=True)
-    # rate = rospy.Rate(.1) # 10hz
+    pub = rospy.Publisher('motor_command', Int32MultiArray, queue_size=10)
+    rospy.init_node('motor_driver', anonymous=True)
+    rate = rospy.Rate(.1) # 10hz
 #  not rospy.is_shutdown
     while True:
         # print(array_of_array[index])
@@ -68,50 +67,67 @@ def motor_driver():
         # rate.sleep()
         time.sleep(1)
 # motors = [5, 6, 7, 8]
-motors = [20 for _ in range (8)]
+motors = [0 for _ in range (8)]
 
 
-pub = rospy.Publisher('motor_command', Int32MultiArray, queue_size=10)
-rospy.init_node('motor_commander', anonymous=True)
-rate = rospy.Rate(.1) # 10hz
+# pub = rospy.Publisher('motor_command', Int32MultiArray, queue_size=10)
+# rospy.init_node('motor_commander', anonymous=True)
+# rate = rospy.Rate(.1) # 10hz
 
 
 
-def jelly_spin(string, seconds, scaler):
-    if (string == "right"):
-        scaler *= -1
-    elif (string == "left"):
-        pass
-    else:
-        return        
-    motors[4] = scaler * -1
-    motors[7] = scaler * -1
-    motors[5] = scaler * 1
-    motors[6] = scaler * 1
+def jelly_turn(direction: str, speed: float, seconds: float):
+    if (direction == "right"):
+        speed *= -1
+    elif (direction != "left"):
+        print("invalid turn direction, try 'right' or 'left'")
+        return
+    motors[4] = speed * -1
+    motors[7] = speed * -1
+    motors[5] = speed * 1
+    motors[6] = speed * 1
+
+    print (motors)
+    time.sleep(seconds)
+    #stopping the turn
     for i in range(4):
         motors[i+4] = 0
-    print (motors)
+    print(motors)
+    # example: jelly_turn("left", 20, 3)
 
-def jelly_go(string, scaler, seconds):
-    if (string == "forwards"): 
-        scaler *= -1
-    else:
-         pass
-    motors[4] = scaler * -1
-    motors[7] = scaler * -1
-    motors[5] = scaler * 1
-    motors[6] = scaler * 1
+def jelly_go(direction: str, speed: float, seconds: float):
+    if (direction == "forwards"): 
+        speed *= -1
+    motors[4] = speed * -1
+    motors[7] = speed * -1
+    motors[5] = speed * 1
+    motors[6] = speed * 1
+    print(motors)
     time.sleep(seconds)
+
+    for i in range(4):
+        motors[i+4] = 0
+    print(motors)
+
 
 def motor_driver(motors: list):
     while not rospy.is_shutdown():
-        pub.publish(data=jelly_spin(motors,'right',50))
+        pub.publish(data=jelly_turn('right', 20, 3))
         rate.sleep(5)
-        pub.publish(data=jelly_go(motors, 'forwards', 50))
+        pub.publish(data=jelly_go('forwards', 10, 3))
         rate.sleep(5)
 
 
+# user_direction = input("State the Direction(forwards or backwords):  ")
+# user_speed = int(input("state the speed(0-80):"))
+# user_seconds = int(input("State the duration of motor usage(int in seconds):"))
 
-jelly_go("forwards", 5, 1)
-print(motors)
+# jelly_go(user_direction, user_speed, user_seconds)
+
+# user_direction = input("State which direction you would like to turn(right or left):  ")
+# user_speed = int(input("state the speed(0-80):"))
+# user_seconds = int(input("State the duration of motor usage(int in seconds):"))
+
+# jelly_turn(user_direction, user_speed, user_seconds)
+
 motor_driver()

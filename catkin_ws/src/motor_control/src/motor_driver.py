114577a1c6bf9f407cf1345e8e7a96caf2e54b35
Max Welge <max@mwc-054024.dhcp.missouri.edu>
2024-10-17 17:34:20 -0500

    Second commit

--- catkin_ws/src/motor_control/src/motor_driver.py
+++ catkin_ws/src/motor_control/src/motor_driver.py
@@ -24,30 +24,6 @@ import rospy
 from std_msgs.msg import Int32MultiArray
 # END IMPORT
 
-'''"
-
-Goals:
-
-Be able to run
-Push something
-Spin up and spin down slowly
-
-Fib seq
-'''
-
-num_motors = 8
-''' 
-array_of_array = [
-    [20 for _ in range(num_motors)],
-    [30 for _ in range(num_motors)],
-    [40 for _ in range(num_motors)],
-    [50 for _ in range(num_motors)],
-    [40 for _ in range(num_motors)],
-    [30 for _ in range(num_motors)],
-    [20 for _ in range(num_motors)],
-    [10 for _ in range(num_motors)]
-]
-'''
 def motor_driver():
 
     index = 0
@@ -63,52 +39,8 @@ def motor_driver():
 
         index = (index + 1) % 8 # iterates 1 through 8
 
-
         # rate.sleep()
         time.sleep(1)
-# motors = [5, 6, 7, 8]
-motors = [0 for _ in range (8)]
-
-
-# pub = rospy.Publisher('motor_command', Int32MultiArray, queue_size=10)
-# rospy.init_node('motor_commander', anonymous=True)
-# rate = rospy.Rate(.1) # 10hz
-
-
-
-def jelly_turn(direction: str, speed: float, seconds: float):
-    if (direction == "right"):
-        speed *= -1
-    elif (direction != "left"):
-        print("invalid turn direction, try 'right' or 'left'")
-        return
-    motors[4] = speed * -1
-    motors[7] = speed * -1
-    motors[5] = speed * 1
-    motors[6] = speed * 1
-
-    print (motors)
-    time.sleep(seconds)
-    #stopping the turn
-    for i in range(4):
-        motors[i+4] = 0
-    print(motors)
-    # example: jelly_turn("left", 20, 3)
-
-def jelly_go(direction: str, speed: float, seconds: float):
-    if (direction == "forwards"): 
-        speed *= -1
-    motors[4] = speed * -1
-    motors[7] = speed * -1
-    motors[5] = speed * 1
-    motors[6] = speed * 1
-    print(motors)
-    time.sleep(seconds)
-
-    for i in range(4):
-        motors[i+4] = 0
-    print(motors)
-
 
 def motor_driver(motors: list):
     while not rospy.is_shutdown():
@@ -116,18 +48,5 @@ def motor_driver(motors: list):
         rate.sleep(5)
         pub.publish(data=jelly_go('forwards', 10, 3))
         rate.sleep(5)
-
-
-# user_direction = input("State the Direction(forwards or backwords):  ")
-# user_speed = int(input("state the speed(0-80):"))
-# user_seconds = int(input("State the duration of motor usage(int in seconds):"))
-
-# jelly_go(user_direction, user_speed, user_seconds)
-
-# user_direction = input("State which direction you would like to turn(right or left):  ")
-# user_speed = int(input("state the speed(0-80):"))
-# user_seconds = int(input("State the duration of motor usage(int in seconds):"))
-
-# jelly_turn(user_direction, user_speed, user_seconds)
 
 motor_driver()
--- catkin_ws/src/motor_control/src/motor_runner.py
+++ catkin_ws/src/motor_control/src/motor_runner.py
@@ -25,8 +25,8 @@ Subscribes:
 """
 
 
-
 # BEGIN IMPORT
+import time
 import rospy
 from std_msgs.msg import Int32MultiArray
 # END IMPORT
@@ -35,9 +35,11 @@ from typing import List
 
 num_motors = 8
 
+'''
 high: List[int] = [20 for i in range(num_motors)]
 low: List[int] = [30 for i in range(num_motors)]
 list_thing = high
+'''
 
 motor_powers = [0 for i in range(num_motors)]
 
@@ -47,15 +49,10 @@ def commander():
     global hl_counter
     pub = rospy.Publisher('motor_command', Int32MultiArray, queue_size=10)
     rospy.init_node('motor_commander', anonymous=True)
-    rate = rospy.Rate(.1) # 10hz
+    rate = rospy.Rate(.4) # 40hz
 
-    #! This is just a section to show the motors running when there is seperate input from ros this should not be used
-    while not rospy.is_shutdown():
-        if (hl_counter == 0):
-            list_thing = high
-        else:
-            list_thing = low
-        hl_counter = (hl_counter + 1) % 2
+
+
         # hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(list_thing)
         pub.publish(data=list_thing)
@@ -66,6 +63,64 @@ def commander():
         print('x')
 
         rate.sleep()
+
+
+motors = [0 for _ in range (8)]
+
+def jelly_turn(direction: str, speed: float, seconds: float):
+    if (direction == "right"):
+        speed *= -1
+    elif (direction != "left"):
+        print("invalid turn direction, try 'right' or 'left'")
+        return
+
+    motors[4] = speed * -1
+    motors[7] = speed * -1
+    motors[5] = speed * 1
+    motors[6] = speed * 1
+
+    print (motors)
+    time.sleep(seconds)
+    #stopping the turn
+    for i in range(4):
+        motors[i+4] = 0
+    print(motors)
+    # example: jelly_turn("left", 20, 3)
+
+def jelly_go(direction: str, speed: float, seconds: float):
+    if (direction == "forwards"): 
+        speed *= -1
+    motors[4] = speed * -1
+    motors[7] = speed * -1
+    motors[5] = speed * 1
+    motors[6] = speed * 1
+    print(motors)
+    time.sleep(seconds)
+
+    for i in range(4):
+        motors[i+4] = 0
+    print(motors)
+
+
+# pub = rospy.Publisher('motor_command', Int32MultiArray, queue_size=10)
+# rospy.init_node('motor_commander', anonymous=True)
+# rate = rospy.Rate(.1) # 10hz
+
+
+# user_direction = input("State the Direction(forwards or backwords):  ")
+# user_speed = int(input("state the speed(0-80):"))
+# user_seconds = int(input("State the duration of motor usage(int in seconds):"))
+
+# jelly_go(user_direction, user_speed, user_seconds)
+
+# user_direction = input("State which direction you would like to turn(right or left):  ")
+# user_speed = int(input("state the speed(0-80):"))
+# user_seconds = int(input("State the duration of motor usage(int in seconds):"))
+
+# jelly_turn(user_direction, user_speed, user_seconds)
+
+
+
 if __name__ == '__main__':
     try:
         commander()

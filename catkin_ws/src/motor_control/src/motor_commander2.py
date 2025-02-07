de2076628c246487ae413bff24dbb138bfaadb32
Elijah VanDyne <114081115+ELVanDyne@users.noreply.github.com>
2025-02-06 18:45:13 -0600

    Charles was right

--- catkin_ws/src/motor_control/src/motor_commander2.py
+++ catkin_ws/src/motor_control/src/motor_commander2.py
@@ -79,10 +79,10 @@ class MotorCommand():
         powers = [x,x,x,x]
         #Code here
         
-        powers[0] = -y+yaw
-        powers[1] = y-yaw
-        powers[2] = -y-yaw
-        powers[3] = y+yaw
+        powers[0] += -y+yaw
+        powers[1] += y-yaw
+        powers[2] += -y-yaw
+        powers[3] += y+yaw
 
         max = 0
         

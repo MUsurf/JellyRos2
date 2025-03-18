5d43e71c95ee4cadf4ab247496d43b5e72a893de
Charles Bacher <teabeignet@gmail.com>
2025-03-18 18:19:13 -0500

    test push :(

--- catkin_ws/src/motor_control/src/PID_commander.py
+++ catkin_ws/src/motor_control/src/PID_commander.py
@@ -78,4 +78,3 @@ class PID_commander:
                     if(abs(thrustList[i]) > self.MAX_THRUST) : thrustList[i] = (abs(thrustList[i]) / thrustList) * self.MAX_THRUST
         
         return thrustList
-    

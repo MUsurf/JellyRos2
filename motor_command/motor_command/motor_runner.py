c00dcad6b23cdc84140995317a73ec6b21c93c36
Creeon <92059871+Creeon@users.noreply.github.com>
2025-09-16 02:55:31 -0500

    Created test code

--- motor_command/motor_command/motor_runner.py
+++ motor_command/motor_command/motor_runner.py
@@ -28,6 +28,7 @@ Subscribes:
 
 # BEGIN IMPORT
 import rclpy
+import time
 from rclpy.node import Node
 from std_msgs.msg import Int32MultiArray
 # END IMPORT
@@ -39,27 +40,47 @@ class MotorCommander(Node):
     def __init__(self):
         super().__init__('motor_commander')
         self.publisher = self.create_publisher(Int32MultiArray, 'motor_command', 10)
-        self.timer = self.create_timer(5.0, self.timer_callback)  # 0.1 Hz
+        self.timer = self.create_timer(0.1, self.timer_callback)  # 10 Hz
         
         self.num_motors = 8
-        self.high: List[int] = [20 for _ in range(self.num_motors)]
-        self.low: List[int] = [30 for _ in range(self.num_motors)]
-        self.list_thing = self.high
-        self.hl_counter = 0
+        self.wait_time : float = 30
+        self.sequence_time : float = 15
+        
+        self.start_time : float = time.time()
+        self.first : bool = True
+        
+        self.motor_sequences : List[int] = [
+            [30.0, 30.0, 30.0, 30.0, 30.0, 30.0, 30.0, 30.0]
+            [0.0, 0.0, 0.0, 0.0, 30.0, 30.0, 30.0, 30.0],
+            [30.0, 30.0, 30.0, 30.0, 0.0, 0.0, 0.0, 0.0]
+        ]
+        self.low: List[int] = [0 for _ in range(self.num_motors)]
+        self.sequence_counter = 0
+        self.activated = False
         
         self.get_logger().info('MotorCommander Node Created')
     
     def timer_callback(self):
-        self.list_thing = self.high if self.hl_counter == 0 else self.low
-        self.hl_counter = (self.hl_counter + 1) % 2
+        if self.first:
+            self.publish_motor_data(self.low)
+            
+            self.first = False
+            self.activated = False
+            self.start_time = time.time()
+            
+        elif time.time() - self.start_time >= (self.sequence_time if self.activated else self.wait_time):
+            self.publish_motor_data(self.low if self.activated else self.motor_sequences[self.sequence_counter%len(self.motor_sequences)])
+            
+            self.motor_sequences += 1
+            self.activated = not self.activated
+            self.start_time = time.time()
         
+    def publish_motor_data(self, data : list):
         msg = Int32MultiArray()
-        msg.data = self.list_thing
-        
+        msg.data = data
         self.get_logger().info(f'Publishing: {msg.data}')
         self.publisher.publish(msg)
-
-
+        
 def main(args=None):
     rclpy.init(args=args)
     node = MotorCommander()
